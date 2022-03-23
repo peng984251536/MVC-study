1eb1d60bfafb2dd283deb7d7c61d6da54f737e25
peng984251536 <984251536@qq.com>
2022-03-23 16:41:12 +0800

    添加 .gitignore 和 .gitattributes。

---
+++ Assets/My/Scripts/Framwork/Facade.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//门面，有点类于单例模式的Manager
+public class Facade : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/My/Scripts/Framwork/ViewBase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//视图类：
+//1、每个类都有自己的名字
+//2、每个类都注册自己关系的视角，并且处理该事件，比如"AttackEvent"、
+public abstract class ViewBase : MonoBehaviour  //抽象类相当于制定了一个模板，规范
+{
+    public abstract string Name { get; }
+
+    public virtual void F() { }
+
+    public abstract void G();
+}
+public class MainView : ViewBase
+{
+    public override string Name
+    {
+        get { return "MainView"; }
+    }
+
+    public override void G()
+    {
+        this.F();
+        throw new System.NotImplementedException();
+    }
+}
